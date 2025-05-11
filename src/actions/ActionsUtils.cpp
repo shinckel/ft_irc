f9c88a1c7d2f63f842c2babbed137cfc469c1fbf
Sofia Hinckel Dias <sofiahdias@gmail.com>
2025-05-11 18:59:58 +0100

    feat: add USER confirmation message

--- src/actions/ActionsUtils.cpp
+++ src/actions/ActionsUtils.cpp
@@ -11,15 +11,10 @@ void Manager::passAction(Client &client) {
     password.erase(0, password.find_first_not_of(" \t\r\n")); // Trim leading whitespace
     password.erase(password.find_last_not_of(" \t\r\n") + 1); // Trim trailing whitespace
 
-    std::cout << "Hexchat password: [" << password << "]" << std::endl;
-    std::cout << "Local server password: [" << _password << "]" << std::endl;
-
     if (password == _password) {
         client.setAuthenticated(true);
-        std::cout << "Password correct for client " << client.getId() << std::endl;
         sendIrcMessage(client.getId(), ":server NOTICE * :Authentication successful");
     } else {
-        std::cout << "Incorrect password for client " << client.getId() << std::endl;
         sendIrcMessage(client.getId(), ":server 464 * :Password incorrect");
     }
 }
@@ -74,22 +69,58 @@ void Manager::sendWhoMessage(const std::vector<int> &list, Client &client, std::
 //     sendIrcMessage(client.getId(), formatMessage(client, RPL_ENDOFWHO) + " " + channelName + " :End of WHO list");
 // }
 
-void Manager::nickAction(Client &client)
-{
-    if (Parser::nickParse(client)) 
-    {
-        if (client.getNickName() != "")
-            sendIrcMessage(client.getId(), ":" + client.getNickName() + " NICK :" + client.getCommand()[1]);
-        else
-            sendIrcMessage(client.getId(), ":" + client.getLastNick() + " NICK :" + client.getCommand()[1]);
-        client.setNickName(client.getCommand()[1]);
+// OLD - SOLID RESULTS, TRY TO IMPROVE IT
+// void Manager::nickAction(Client &client)
+// {
+//     if (Parser::nickParse(client)) 
+//     {
+//         if (client.getNickName() != "")
+//             sendIrcMessage(client.getId(), ":" + client.getNickName() + " NICK :" + client.getCommand()[1]);
+//         else
+//             sendIrcMessage(client.getId(), ":" + client.getLastNick() + " NICK :" + client.getCommand()[1]);
+//         client.setNickName(client.getCommand()[1]);
+//     }
+// }
+
+// IMPROVEMENT??
+void Manager::nickAction(Client &client) {
+    if (Parser::nickParse(client)) {
+        std::string oldNick = client.getNickName();
+        std::string newNick = client.getCommand()[1];
+
+        client.setNickName(newNick);
+
+        // If the client already has a nickname, notify about the change
+        if (!oldNick.empty()) {
+            sendIrcMessage(client.getId(), ":" + oldNick + "!" + client.getUserName() + "@localhost NICK :" + client.getNickName()); // Notify all clients in the same channels
+        } else {
+            // If this is the first time setting a nickname, send a welcome message
+            sendIrcMessage(client.getId(), ":localhost 001 " + newNick + " :Welcome to the IRC network, " + newNick);
+        }
+    } else {
+        sendIrcMessage(client.getId(), ":localhost 432 * " + client.getCommand()[1] + " :Erroneous nickname");
     }
 }
 
+// void Manager::userAction(Client &client) {
+//     if (Parser::userParse(client)) {
+//         std::vector<std::string> command = client.getCommand();
+//         std::string username = command[1].substr(0, command[1].find(" "));
+//         client.setUserName(username);
+//         sendIrcMessage(client.getId(), ":" + client.getNickName() + "!" + client.getUserName() + "@localhost USER :User created");
+//     }
+// }
 void Manager::userAction(Client &client) {
     if (Parser::userParse(client)) {
-        std::vector<std::string> command = client.getCommand();
-        std::string username = command[1].substr(0, command[1].find(" "));
-        client.setUserName(username);
+        if (client.getNickName().empty()) {
+            sendIrcMessage(client.getId(), ":localhost NOTICE * :You must set a nickname using the NICK command.");
+        } else {
+            std::vector<std::string> command = client.getCommand();
+            std::string username = command[1].substr(0, command[1].find(" "));
+            client.setUserName(username);
+            sendIrcMessage(client.getId(), ":server NOTICE * :User " + username + " has been created successfully.");
+        }
+    } else {
+        sendIrcMessage(client.getId(), ":localhost 461 USER :Invalid USER command");
     }
 }
